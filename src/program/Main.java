8f73952ee00363a31047dff73d33724397bee73a
Nicolas <nicolas.soaresh.98@gmail.com>
2019-10-31 19:34:05 -0300

    Apenas teste

--- src/program/Main.java
+++ src/program/Main.java
@@ -10,10 +10,7 @@ public class Main {
 
 	public static void main(String[] args) throws ParseException {
 
-		ClienteDao c = DaoFactory.createClienteDao();
-		Cliente c1 = c.findById(1);
-		
-		System.out.println(c1);
+		//teste
 	
 	}
 
