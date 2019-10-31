52dc91400f1265e6aff9e4f1a6cfa82fc9e88dc6
Nicolas <nicolas.soaresh.98@gmail.com>
2019-10-31 10:22:11 -0300

    Alterado configurações de acesso ao banco

--- src/program/Main.java
+++ src/program/Main.java
@@ -2,12 +2,18 @@ package program;
 
 import java.text.ParseException;
 
+import model.dao.ClienteDao;
+import model.dao.DaoFactory;
+import model.entities.Cliente;
+
 public class Main {
 
 	public static void main(String[] args) throws ParseException {
 
-		//instruções SQL foram feitas de maneira genérica, isto é, não serão utilizadas especificamente estas 
-		//instruções para a APS e sim modificações delas que atendam melhor aos requisitos do programa
+		ClienteDao c = DaoFactory.createClienteDao();
+		Cliente c1 = c.findById(1);
+		
+		System.out.println(c1);
 	
 	}
 
